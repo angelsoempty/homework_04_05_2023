e81c0e0e54483fe29004fd2d6db8c38bc6f0fccc
angelsoempty <denatamanov1@gmail.com>
2023-05-09 02:48:20 +0300

    #3

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-def increase_result_by_100(func):
-    def wrapper(numbers):
-        result = func(numbers)
-        return result + 100
+def log_decorator(func):
+    def wrapper(celsius):
+        result = func(celsius)
+        print(f"Перетворення температури: {celsius}C -> {result}F")
+        return result
     return wrapper
 
-@increase_result_by_100
-def multiply_numbers(numbers):
-    result = 1
-    for num in numbers:
-        result *= num
-    return result
-
-numbers_list = [1, 2, 3]
-result = multiply_numbers(numbers_list)
-print(f"Добуток чисел {numbers_list} збільшений на 100: {result}")
+@log_decorator
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+temperature_celsius = 25
+temperature_fahrenheit = celsius_to_fahrenheit(temperature_celsius)
+print(f"Температура {temperature_celsius}C дорівнює {temperature_fahrenheit}F")
